7a5645b8a24aee98cdde0714ffcf11355bcc702a
Jan Dobes <jdobes@redhat.com>
2020-03-26 22:31:19 +0000

    fix for organization owner type and add more debug msgs

--- secrets_broker/api.py
+++ secrets_broker/api.py
@@ -56,29 +56,40 @@ def secrets():
     if not secrets_request:
         return {"error": "Secrets request not initialized."}, 403
 
-    # check github API about run origin
+    # check github API about action run initiator
     response = requests.get("%s%s" % (GITHUB_API, GITHUB_API_RUN_ENDPOINT % (repo, run_id)),
                             headers={"Authorization": "token %s" % gh_token})
     if response.status_code != 200:
+        LOGGER.debug("Invalid HTTP code from Github: %s", response.status_code)
         return {"error": "Validation failed."}, 403
     run_detail = response.json()
     head_repository_name = run_detail["head_repository"]["full_name"]
-    user_login = run_detail["head_repository"]["owner"]["login"]
+    owner_login = run_detail["head_repository"]["owner"]["login"]
+    owner_type = run_detail["head_repository"]["owner"]["type"].lower()
     if head_repository_name == repo:
-        LOGGER.debug("Initiator's head repo is allowed repo: %s" % repo)
-    elif user_login in ALLOWED_GITHUB_USERS:
-        LOGGER.debug("Initiator's head repo owner is in alowed users: %s." % user_login)
-    else:  # check user's org
-        response = requests.get("%s%s" % (GITHUB_API, GITHUB_API_USER_ORGS_ENDPOINT % (user_login)),
+        LOGGER.debug("Initiator's head repo is allowed repo: %s", repo)
+    elif owner_type == "user" and owner_login in ALLOWED_GITHUB_USERS:
+        LOGGER.debug("Initiator's head repo owner is in allowed users: %s.", owner_login)
+    elif owner_type == "organization" and owner_login in ALLOWED_GITHUB_ORGS:
+        LOGGER.debug("Initiator's head repo owner is in allowed orgs: %s.", owner_login)
+    elif owner_type == "user":  # check user's org
+        response = requests.get("%s%s" % (GITHUB_API, GITHUB_API_USER_ORGS_ENDPOINT % (owner_login)),
                                 headers={"Authorization": "token %s" % gh_token})
         if response.status_code != 200:
+            LOGGER.debug("Invalid HTTP code from Github: %s", response.status_code)
             return {"error": "Validation failed."}, 403
         org_list = response.json()
         valid_orgs = [org["login"] for org in org_list if org["login"] in ALLOWED_GITHUB_ORGS]
         if not valid_orgs:
+            LOGGER.debug("Initiator's head repo owner is NOT a member of allowed orgs: %s.", owner_login)
             return {"error": "Validation failed."}, 403
+        LOGGER.debug("Initiator's head repo owner is a member of allowed orgs: %s.", owner_login)
+    else:
+        LOGGER.debug("Permission denied for owner: login=%s, type=%s.", owner_login, owner_type)
+        return {"error": "Validation failed."}, 403
 
     # TODO: check token in log
 
+    LOGGER.debug("Permission granted: login=%s, type=%s.", owner_login, owner_type)
     requested_keys = [key.strip() for key in connexion.request.args["keys"].split(",") if key.strip()]
     return [{"key": key, "value": SECRETS[key]} for key in requested_keys if key in SECRETS]
